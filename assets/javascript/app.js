2bde98a80ab011653e9745c35543de0412451a26
Jake P <93plaistj@protonmail.ch>
2018-07-20 17:26:31 -0400

    everything seems to be working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,6 +21,7 @@ $(function () {
                 .text(gifArr[j]);
 
             $('.buttons').append(btns);
+            $('.buttons').fadeIn("slow");
            // btnData  = $(this).attr("data-name");
         }
     } 
@@ -37,47 +38,48 @@ $(function () {
 
 
 
-    $("div").on('click', function (event){ 
+    //$(".gif").on('click', function (event){ 
+        var get_gif = function(){
         //push_buttons();
-        //$IMG.empty(); 
+        $('.gifIMG').empty();
          btnData  = $(this).attr("data-name");
         var queryURL = "http://api.giphy.com/v1/gifs/search?q="
             + btnData + "&apikey=" + KEY + "&limit=5";
-          //  console.log(queryURL);
-           // console.log(btnData);
+            console.log(queryURL);
+            console.log(btnData);
         event.preventDefault();
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response){
             var results = response.data;
-            
-            
+            $('#gif-view').empty();
+            if (results == "" ){
+                alert("Gifs not available")
+            }
             for (var i = 0; i < results.length; i++){
                
-                //console.log(btnData)
-                var gifDiv = $("<div>");
+                console.log(btnData)
+                var gifDiv = $("<div>");        //appends Rating of each Gif
                 var p = $("<p>").text("Gif Rating: " + results[i].rating);
                 var $IMG = $("<img>");
-
-                $IMG.attr("src", results[i].images.original_still.url)
-                    .attr("data-still", results[i].images.original_still.url)
-                    .attr("data-animate", results[i].images.original.url)
+                                            //sets the height of the api results to small, to keep uniformed
+                $IMG.attr("src", results[i].images.fixed_height_small_still.url)
+                    .attr("data-still", results[i].images.fixed_height_small_still.url)
+                    .attr("data-animate", results[i].images.fixed_height_small.url)
                     .attr("data-state","still");
                 $IMG.attr("class", "gifIMG");
                 gifDiv.append(p);
                 gifDiv.append($IMG);
 
-                $('.buttons').append(gifDiv);
+                $('#gif-view').append(gifDiv);
+                $('#gif-view').fadeIn("slow");
+                console.log(results[i].rating);
             }
         })
-    });
+    };
 
-    $('button').on('click', function(){
-        console.log("clicked");
-        $(".gifDiv").empty();
-    })
-
+    //function to pause / unpause gifs by user
 	var gif_state = function(){
         console.log('wtf')
         //console.log(this);
@@ -97,9 +99,30 @@ $(function () {
             $(this).attr("data-state", "still");
             console.log('con2')
 		}
-	}
+    }
+    
+    var gif_states = function(){
+        var state = $(this).attr("src");
+        var animated = $(this).attr("data-animate");
+        var still = $(this).attr("data-still");
+        console.log('WE MADE IT')
+
+        switch (state){
+            case still:
+                console.log('case 1');
+                $(this).attr("src",animated);
+                $(this).attr("data-state", "animate");
+                break;
+            case animated:
+            $(this).attr("src", still);
+            $(this).attr("data-state", "still");
+                console.log('case 2');
+                break;
+        }
+    }
 
 
-    $(document).on('click', '.gifIMG', gif_state);
+    $(document).on('click', '.gifIMG', gif_states);
+    $(document).on('click', '.gif', get_gif);
 
 });
