a7339e74be28e92a0a6093cff71109808d5923b3
Jake P <93plaistj@protonmail.ch>
2018-07-20 15:34:57 -0400

    some working buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,10 +17,11 @@ $(function () {
 
             var btns = $('<button>');
             btns.addClass('gif')    //adds class of gif
-                .attr('data-gif', gifArr[j])
+                .attr('data-name', gifArr[j])
                 .text(gifArr[j]);
 
             $('.buttons').append(btns);
+           // btnData  = $(this).attr("data-name");
         }
     } 
     push_buttons();
@@ -28,7 +29,7 @@ $(function () {
     //function to append user input to button list    
     $("#addGif").on("click", function(event){
         event.preventDefault();     //prevents from opening in new page?
-
+        
         var x = $('#gif-input').val().trim(); //appends the text to the new button
         gifArr.push(x);  //pushes the user input into the Gif array
         push_buttons();
@@ -36,30 +37,33 @@ $(function () {
 
 
 
-    $(".buttons").on('click', function (){
-        var btnData  = $(this).attr("data-gif");
+    $("div").on('click', function (event){ 
+        //push_buttons();
+        //$IMG.empty(); 
+         btnData  = $(this).attr("data-name");
         var queryURL = "http://api.giphy.com/v1/gifs/search?q="
             + btnData + "&apikey=" + KEY + "&limit=5";
-        
-            console.log(btnData);
-
+          //  console.log(queryURL);
+           // console.log(btnData);
+        event.preventDefault();
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response){
             var results = response.data;
-            $('.btns').empty();
-
+            
+            
             for (var i = 0; i < results.length; i++){
-                console.log(btnData)
+               
+                //console.log(btnData)
                 var gifDiv = $("<div>");
                 var p = $("<p>").text("Gif Rating: " + results[i].rating);
                 var $IMG = $("<img>");
 
                 $IMG.attr("src", results[i].images.original_still.url)
-                $IMG.attr("data-still", results[i].images.original_still.url)
-                $IMG.attr("data-animate", results[i].images.original.url);
-                $IMG.attr("data-state","still");
+                    .attr("data-still", results[i].images.original_still.url)
+                    .attr("data-animate", results[i].images.original.url)
+                    .attr("data-state","still");
                 $IMG.attr("class", "gifIMG");
                 gifDiv.append(p);
                 gifDiv.append($IMG);
@@ -67,26 +71,12 @@ $(function () {
                 $('.buttons').append(gifDiv);
             }
         })
+    });
+
+    $('button').on('click', function(){
+        console.log("clicked");
+        $(".gifDiv").empty();
     })
-
-
-    /*var gif_state = function(){
-        var state = $(this).attr("data-state");
-        var animated = $(this).attr("data-animate");
-        var still = $(this).attr("data-still");
-
-        if (state == "still"){
-            $(this).attr("src", animated);
-            $(this).attr("data-state", "animate");
-            console.log('unstill');
-        }
-        else if (state == "animate") {
-            $(this).attr("src", still);
-            $(this).attr("data-state", "still");
-            console.log('de-animate');
-        }
-        console.log('checked');
-    }*/
 
 	var gif_state = function(){
         console.log('wtf')
